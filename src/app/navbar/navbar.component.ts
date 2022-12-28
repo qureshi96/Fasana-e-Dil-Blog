53da45655becabe86b2847b03f3ac497642adcd0
qureshi96 <usmanramses@gmail.com>
2022-12-28 04:19:36 +0000

    Typewriter animation for title
    animation on title implemented

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import Typewriter from 't-writer.js';
 
 
 @Component({
@@ -14,7 +14,87 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const target = document.querySelector('.tw');
+    const writer = new Typewriter(target, {
+      loop: true,
+      typeSpeed: 50,
+      deleteSpeed: 50,
+      cursorColor: '#7A1A1A',
+      typeColor: '#7A1A1A'
+      
+    })
     
+    writer
+      .changeTypeClass('urdufont')
+      .type('فسانہِ دِل')
+      .rest(1500)
+      .clear()
+      .type('fabulam cordis')
+      .rest(1500)
+      .clear()
+      .type('فسانہِ دِل')
+      .rest(1500)
+      .clear()
+      .type('priča o srcu')
+      .rest(1500)
+      .clear()
+      .type('فسانہِ دِل')
+      .rest(1500)
+      .clear()
+      .type('قصة القلب')
+      .rest(1500)
+      .clear()
+      .type('فسانہِ دِل')
+      .rest(1500)
+      .clear()
+      .changeTypeClass('cyrillic')
+      .type('история о сердце')
+      .rest(1500)
+      .clear()
+      .changeTypeClass('urdufont')
+      .type('فسانہِ دِل')
+      .rest(1500)
+      .clear()
+      .type('داستان قلب')
+      .rest(1500)
+      .clear()
+      .type('فسانہِ دِل')
+      .rest(1500)
+      .clear()
+      .type('kalbin hikayesi')
+      .rest(1500)
+      .clear()
+      .type('فسانہِ دِل')
+      .rest(1500)
+      .clear()
+      .type('दिल की कहानी')
+      .rest(1500)
+      .clear()
+      .type('فسانہِ دِل')
+      .rest(1500)
+      .clear()
+      .type("סיפור הלב")
+      .rest(1500)
+      .clear()
+      .type('فسانہِ دِل')
+      .rest(1500)
+      .clear()
+      .type("心的故事")
+      .rest(1500)
+      .clear()
+      .type('فسانہِ دِل')
+      .rest(1500)
+      .clear()
+      .type("心の物語")
+      .rest(1500)
+      .clear()
+      .type('فسانہِ دِل')
+      .rest(1500)
+      .clear()
+      .type("გულის ამბავი")
+      .rest(1500)
+      .clear()
+      .start()
     
   }
   
